42034a810de77754e220291b787033a5deb4e901
Ayush Kumar Sethi <ayush.sethi@apty.io>
2023-08-23 20:18:08 +0530

    New Client-SPM version 1.0.0

---
+++ Package.swift
@@ -0,0 +1,31 @@
+// swift-tools-version: 5.8
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let version = "1.0.0"
+let moduleName = "Client-SPM"
+let checksum = ""
+let url = "https://github.com/ayush-apty/Client-SPM-Dist/releases/download/\(version)/\(moduleName).xcframework.zip"
+
+let package = Package(
+    name: moduleName,
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(name: moduleName, targets: [moduleName])
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/mobillium/MobilliumQRCodeReader.git", from: "1.0.0"),
+        .package(url: "https://github.com/socketio/socket.io-client-swift", branch: "master")
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .binaryTarget(name: moduleName, url: url, checksum: checksum)
+    ]
+)
---
+++ build.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+# Check for arguments
+if [ $# -eq 0 ]; then
+    echo "No argument provided. Argument has to be version, e.g. '1.1.1'"
+    exit 1
+fi
+
+# 1. Get params
+NEW_VERSION=$1
+
+# 2. Calculate checksum and store it
+echo "Calculate new checksum"
+NEW_CHECKSUM=$(swift package compute-checksum Framework.zip)
+echo "print out new shasum for convenience reasons"
+echo "New checksum is $NEW_CHECKSUM"
+
+# 3. Replace all data from Package.swift manifest
+echo "replace version information in package manifest"
+sed -E -i '' 's/let version = ".+"/let version = "'$NEW_VERSION\"/ Package.swift
+echo "replace checksum information in package manifest"
+sed -E -i '' 's/let checksum = ".+"/let checksum = "'$NEW_CHECKSUM\"/ Package.swift
+
+# 4. Print new content of manifest
+echo "print out package manifest for convenience reasons"
+cat Package.swift
+
+# 5. Commit all data without the XCfrmework
+echo "git commit all without framework and push"
+git add --all -- :!Frameworks.zip
+git commit -m "New Client-SPM version $NEW_VERSION"
+git push
+
+# 6. Pusblish a new release with the same version of the repository A, and attach XCFramework in the Release metadata
+echo "Releasing the new version"
+gh release create "$NEW_VERSION" --generate-notes "./Frameworks.zip"
+
+# 7. Remove zip of XCFramework
+echo "delete downloaded zip file"
+rm Frameworks.zip
